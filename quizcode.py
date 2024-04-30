0d044a8ead6ed8a089f331ebec06cb92a61d4e1f
Prisha1509 <168353255+Prisha1509@users.noreply.github.com>
2024-04-29 16:44:04 -1200

    Update quizcode.py

--- quizcode.py
+++ quizcode.py
@@ -2,3 +2,12 @@ from tkinter import *
 from tkinter.ttk import Progressbar
 import pyttsx3
 from pygame import mixer
+
+voice_sample = pyttsx3.init()
+voices=voice_sample.getProperty('voices')
+voice_sample.setProperty('voice',voices[1].id)
+
+#load the background music 
+mixer.init()
+mixer.music.load('kbc.mp3')
+mixer.music.play(-1)
