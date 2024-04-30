061f04e208dbaee553adc39e9f18b6083b00103e
Prisha1509 <168353255+Prisha1509@users.noreply.github.com>
2024-04-29 16:45:44 -1200

    Update quizcode.py

--- quizcode.py
+++ quizcode.py
@@ -11,3 +11,17 @@ voice_sample.setProperty('voice',voices[1].id)
 mixer.init()
 mixer.music.load('kbc.mp3')
 mixer.music.play(-1)
+
+def choice(action):
+    select_call_button.place_forget()
+    statusbarA.place_forget()
+    statusbarB.place_forget()
+    statusbarC.place_forget()
+    statusbarD.place_forget()
+
+    statusbarLabelA.place_forget()
+    statusbarLabelB.place_forget()
+    statusbarLabelC.place_forget()
+    statusbarLabelD.place_forget()
+    b = action.widget
+    data = b['text']
